f8dad9e15482084b43d001eec12a498903de03cb
Saahil Ognawala <saahil.in@gmail.com>
2016-05-19 14:45:09 +0200

    removed some extra files

--- test_analyze.py
+++
@@ -1,4 +0,0 @@
-from analyze_klee_test import find_ptr_errs, get_culp_line, find_matching_error
-
-err_funcs = find_ptr_errs('/home/ognawala/stonesoup/stonesoup-c-mc/bzip2/blocksort')
-print err_funcs
