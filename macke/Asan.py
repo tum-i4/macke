5fa0c7d471b580e7c0bee4b6a17ddb28f48e9000
Fabian Kilger <fabian.kilger@cssbook.de>
2018-06-07 15:49:33 +0200

    fixed stupid bug
    Signed-off-by: Fabian Kilger <fabian.kilger@cssbook.de>

--- macke/Asan.py
+++ macke/Asan.py
@@ -47,7 +47,7 @@ class AsanResult:
                 description = line[line.find(b"Sanitizer:")+11:]
                 description.strip()
                 desc_parts = description.split(b' ')
-                if b'on' in self.description:
+                if b'on' in description:
                     self.description = b' '.join(desc_parts[0:desc_parts.index(b'on')]).decode("utf-8")
                 else:
                     self.description = desc_parts[0].decode("utf-8", 'ignore').rstrip(':')
